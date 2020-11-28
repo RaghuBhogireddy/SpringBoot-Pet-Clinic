11857252ac91b9233f0105c0c27162422f1b0d6e
RaghuBhogireddy <raghurambhogireddy@gmail.com>
2020-11-28 15:28:09 +0530

    Refactor done for Service Interface to Common Base Interface. Closes #15.

--- pet-clinic-data/src/main/java/io/rrb/petclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/io/rrb/petclinic/service/PetService.java
@@ -2,10 +2,5 @@ package io.rrb.petclinic.service;
 
 import io.rrb.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet Save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 }
